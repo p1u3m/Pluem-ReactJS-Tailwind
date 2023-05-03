3dc70f371942acd864a7f75b64a2f58f43885bf5
Pluem <pluem_544@hotmail.com>
2023-05-04 02:31:41 +0700

    zz

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,7 +3,7 @@ export { default as BorderDash } from '../components/BorderDash';
 import { BorderDash } from '../components/BorderDash'
 
 import React from 'react'
-import { HeaderProjects } from '../components/headerprojects'
+import { HeaderProjects } from '../components/HeaderProjects'
 import { BorderDash } from '../components/BorderDash'
 
 function Project() {
