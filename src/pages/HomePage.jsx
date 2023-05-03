1ec8814d68c4dfe1c0e830b0b30821fa687b37b1
Pluem <pluem_544@hotmail.com>
2023-05-04 00:56:06 +0700

    fixed 1

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderHome from '../components/Headerhome'
+import MyGallery from '../components/MyGallery'
+
+function HomePage() {
+  return (
+    <>
+    <HeaderHome/>
+    <MyGallery/>
+    </>
+  )
+}
+
+export default HomePage
---
+++ src/pages/Project.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderProjects from '../components/headerprojects'
+import BorderDash from '../components/BorderDash'
+
+function Project() {
+  return (
+    <>
+    <HeaderProjects/>
+    
+    <BorderDash/>
+    </>
+  )
+}
+
+export default Project
