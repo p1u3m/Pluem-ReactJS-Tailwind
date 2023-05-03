ffc8d58d8d2a00616bb1ba1eb51a32b0bc3f1853
Pluem <pluem_544@hotmail.com>
2023-05-03 14:31:23 +0700

    commit

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderHome from '../components/HeaderHome'
+import MyGallery from '../components/MyGallery'
+import MyGallery2 from '../components/MyGallery2'
+
+function HomePage() {
+  return (
+    <>
+    <HeaderHome/>
+    <MyGallery2/>
+    </>
+  )
+}
+
+export default HomePage
---
+++ src/pages/project.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeaderProjects from '../components/HeaderProjects'
+import BorderDash from '../components/BorderDash'
+
+function Project() {
+  return (
+    <>
+    <HeaderProjects/>
+    <div className=''></div>
+    <BorderDash/>
+    </>
+  )
+}
+
+export default Project
