b93760e4d8d450add249e65176261c41c490e215
Pluem <pluem_544@hotmail.com>
2023-05-04 02:21:53 +0700

    index

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as HeaderDashboard } from './HeaderDashboard';
+export { default as BorderDash } from './BorderDash';
+export { default as ErrorText } from './ErrorText';
+export { default as HeaderHome } from './HeaderHome';
+export { default as HeaderProjects } from './HeaderProjects';
+export { default as MyGallery } from './MyGallery';
+export { default as Navbar } from './Navbar';
