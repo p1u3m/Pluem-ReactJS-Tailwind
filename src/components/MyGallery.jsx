f1d9931f5ed1d92498c40c8cc0a14969f24dffed
Pluem <pluem_544@hotmail.com>
2023-05-04 13:06:18 +0700

    pic move

--- src/components/MyGallery.jsx
+++ src/components/MyGallery.jsx
@@ -5,23 +5,23 @@ function MyGallery() {
     <div className='mx-auto max-w-5xl flex py-10 px-5'>
     <div className='grid grid-cols-3 gap-4'>
   <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./1.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/1.png"></img></div>
   <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./2.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/2.png"></img></div>
   <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./3.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/3.png"></img></div>
   <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./4.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/4.png"></img></div>
     <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./5.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/5.png"></img></div>
     <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./6.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/6.png"></img></div>
     <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./7.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/7.png"></img></div>
     <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./8.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/8.png"></img></div>
     <div>
-    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./9.png"></img></div>
+    <img className='hover:opacity-50 text-3xl rounded-xl shadow-lg transition duration-150' src="./src/assets/9.png"></img></div>
 </div>
 </div>
   )
