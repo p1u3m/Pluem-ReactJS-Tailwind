021c2de63413bad58ff1baff0b2dc92887c1eb5e
Pluem <pluem_544@hotmail.com>
2023-05-04 13:47:08 +0700

    zxzxzx

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import HeaderDashboard from '../components/HeaderDashboard'
-import BorderDash from '../components/BorderDash'
+import HeaderDashboard from './components/HeaderDashboard'
+import BorderDash from './components/BorderDash'
 
 function Dashboard() {
   return (
